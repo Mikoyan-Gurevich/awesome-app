cdaedc35fda08223f515e00a702dc09688809463
Gaurav Sharma <gaurav.min.js@gmail.com>
2018-02-06 22:08:11 +0530

    Initial Commit

---
+++ src/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo"></image>
+    <text class="ownerInfo">Joshi bhaiya is the best</text>
+    <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .ownerInfo {
+    margin-top: 70px;
+    font-size: 30px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
